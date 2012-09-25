ded1a978b49f24a774556d8a8f6b57998ef52c95
Esa-Matti Suuronen <esa-matti@suuronen.org>
2012-09-25 13:56:04 +0300

    Add app update tool

---
+++ bin/app-update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+cd $(git rev-parse --show-toplevel)
+
+if [[ ! $(cat .git/HEAD) =~ "ref: refs/heads/master"  ]]; then
+    echo "You are not on the master branch?"
+    exit 1
+fi
+
+APP=$(basename $(pwd))
+
+git fetch origin
+
+git branch -r
+
+read -p "Branch?> " BRANCH
+
+[ -z "$BRANCH" ] && exit 1
+
+git diff master..$BRANCH
+
+builtin read -p "Merge above and update? (y/n)> " MERGE
+if [ "$MERGE" != "y" ]; then
+    echo "Merge canceled"
+    exit 1
+fi
+
+git merge $BRANCH
+make
+sudo restart $APP
+sudo tail -f /var/log/upstart/$APP.log ; echo && echo "Push master if ok: git push origin master:master"
